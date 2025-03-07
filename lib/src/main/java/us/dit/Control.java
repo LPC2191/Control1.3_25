bc3b046bd1d46a6703740bf860a16233ed8866e7
salas <salas@trajano.us.es>
2025-03-07 09:16:44 +0100

    nueva version commit

--- lib/src/main/java/us/dit/Control.java
+++ lib/src/main/java/us/dit/Control.java
@@ -1,1 +1,26 @@
+//uvus:lpc2191
+//pass:1741335161
 
+package us.dit;
+
+public class Control {
+
+    public static void main(String[] args) {
+        int n = 10; // Número de términos que quieres generar en la sucesión
+
+        System.out.println("Sucesión de Fibonacci hasta el término " + n + ":");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    // Método recursivo para calcular el enésimo término de la sucesión de Fibonacci
+    public static int fibonacci(int num) {
+        if (num <= 1) {
+            return num;
+        } else {
+            return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+}
